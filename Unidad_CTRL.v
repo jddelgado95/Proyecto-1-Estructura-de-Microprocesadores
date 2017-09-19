2ea0d4a109c14be682a3cf841310cad2530f82d2
josenv27 <josen-27@hotmail.com>
2017-09-19 12:55:03 -0600

    Update Unidad_CTRL.v

--- Unidad_CTRL.v
+++ Unidad_CTRL.v
@@ -19,7 +19,7 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+//modulo para la unidad de control de las señales del microprocesador en general
 module Unidad_CTRL(
 RegDst,
 ALUSrc,
@@ -41,42 +41,47 @@ reg RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,Jump,SignZero;
 reg [1:0] ALUOp;
 always @(*)
 casex (Opcode)
+ //para las instrucciones tipo R
  6'b000000 : begin // R - type
-     RegDst = 1'b1;
-     ALUSrc = 1'b0;
+     RegDst = 1'b1; //activa el banco de registros
+     ALUSrc = 1'b0; 
      MemtoReg= 1'b0;
-     RegWrite= 1'b1;
+     RegWrite= 1'b1; //escribe en el banco de registros
      MemRead = 1'b0;
      MemWrite= 1'b0;
      Branch = 1'b0;
-     ALUOp = 2'b10;
+     ALUOp = 2'b10; //la ALU se activa para hacer operación lógica/aritmética
      Jump = 1'b0;
      SignZero= 1'b0;
     end
+ //para las instrucciones que cargan un dato de la memoria
  6'b100011 : begin // lw - load word
      RegDst = 1'b0;
-     ALUSrc = 1'b1;
-     MemtoReg= 1'b1;
-     RegWrite= 1'b1;
-     MemRead = 1'b1;
+     ALUSrc = 1'b1; //se activa el RISC de la ALU
+     MemtoReg= 1'b1; //Busca el dato en la memoria para escribirlo en el banco de registros
+     RegWrite= 1'b1; //escribe el dato en el banco de registros
+     MemRead = 1'b1; //lee el dato de la memoria
      MemWrite= 1'b0;
      Branch = 1'b0;
      ALUOp = 2'b00;
      Jump = 1'b0;
      SignZero= 1'b0; // sign extend
     end
+ //para cuando se va a cargar un archivo en la memoria
+ //recordar que "x" significa que no importa el estado
  6'b101011 : begin // sw - store word
      RegDst = 1'bx;
-     ALUSrc = 1'b1;
+     ALUSrc = 1'b1; //se activa el RISC de la ALU
      MemtoReg= 1'bx;
      RegWrite= 1'b0;
      MemRead = 1'b0;
-     MemWrite= 1'b1;
+     MemWrite= 1'b1; //escribe el dato en la memoria
      Branch = 1'b0;
      ALUOp = 2'b00;
      Jump = 1'b0;
      SignZero= 1'b0;
     end
+ //para las intrucciones que hacen branches
  6'b000101 : begin // bne - branch if not equal
      RegDst = 1'b0;
      ALUSrc = 1'b0;
@@ -84,23 +89,27 @@ casex (Opcode)
      RegWrite= 1'b0;
      MemRead = 1'b0;
      MemWrite= 1'b0;
-     Branch = 1'b1;
+     Branch = 1'b1; //hace el branch
      ALUOp = 2'b01;
      Jump = 1'b0;
      SignZero= 1'b0; // sign extend
     end
+ 
+ //para las instrucciones que necesitan un dato inmediato
  6'b001110 : begin // XORI - XOR immidiate
      RegDst = 1'b0;
-     ALUSrc = 1'b1;
+     ALUSrc = 1'b1; //se activda el RISC de la ALU
      MemtoReg= 1'b0;
-     RegWrite= 1'b1;
+     RegWrite= 1'b1; //escribe en el banco de registros
      MemRead = 1'b0;
      MemWrite= 1'b0;
      Branch = 1'b0;
      ALUOp = 2'b11;
      Jump = 1'b0;
-     SignZero= 1'b1; // zero extend
+     SignZero= 1'b1; // hace la extensión de zero
     end
+ 
+ //para las instrucciones que requieren saltos
  6'b000010 : begin // j - Jump
      RegDst = 1'b0;
      ALUSrc = 1'b0;
@@ -110,7 +119,7 @@ casex (Opcode)
      MemWrite= 1'b0;
      Branch = 1'b0;
      ALUOp = 2'b00;
-     Jump = 1'b1;
+     Jump = 1'b1; //hace el salto 
      SignZero= 1'b0;
     end
  default : begin 
